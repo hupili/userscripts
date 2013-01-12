3c00a2092b3326c6cb9774e68ab0c7765b714c3e
hupili <hpl1989@gmail.com>
2013-01-12 16:07:00 +0800

    for qsh bbs

---
+++ qshbbs_url_switch.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           qshbbs_url_switch
+// @namespace      http://hupili.github.com/
+// @description    switch from the internal url to external url of qsh bbs 
+// @include        http://bbs.stuhome.net/*
+// ==/UserScript==
+
+window.location.href = window.location.href.replace("bbs.stuhome.net","bbs.qshpan.com") ;
+
